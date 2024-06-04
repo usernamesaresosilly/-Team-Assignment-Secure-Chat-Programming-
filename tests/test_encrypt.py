6620fe9715fbc7e1f47a27861ecb19b88a3d3994
mh <106450840+usernamesaresosilly@users.noreply.github.com>
2024-06-03 23:44:21 -0700

    First test

--- tests/test_encrypt.py
+++ tests/test_encrypt.py
@@ -6,12 +6,9 @@ from lib import crypt1
 def test_encryption():
     key = Fernet.generate_key()
     msg = b"This is a test message."
-
     crypted = crypt1.symmetric_encrypt(key, msg)
     decrypted = crypt1.symmetric_decrypt(key, crypted)
     assert decrypted == msg, "Decrypted message does not match the original message."
-
-    print("Encryption and decryption test passed.")
 
 
 if __name__ == "__main__":
